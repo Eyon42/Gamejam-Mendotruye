2bd3b51c5502269bce8682d2c86ccf3cbc782494
Alvaro Martinez <alvaror@epidataconsulting.com>
2024-08-10 17:58:24 -0300

    BuildingTile calculo ganancias y perdidas

--- BuildingTile.gd
+++ BuildingTile.gd
@@ -1,6 +1,8 @@
 class_name BuildingTile
 extends Tile
 
+
+
 enum Category {
 	RESIDENCIAL,
 	RECREATIVO,
@@ -8,13 +10,55 @@ enum Category {
 	COMERCIAL
 }
 
+@export var aumento : float = 50
+@export var aumento_grande : float = 100
+@export var resta : float = -50
+@export var resta_grande : float = -100
+
 @export var category: Category
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _calculate_money_gain(categoria: Category , categoria_vecina: Category):
+	var money_changed 
+	if categoria == Category.RESIDENCIAL:
+		if categoria_vecina == Category.RECREATIVO:
+			money_changed = aumento_grande
+		if categoria_vecina == Category.INDUSTRIAL:
+			money_changed = resta_grande
+		if categoria_vecina == Category.COMERCIAL:
+			money_changed = aumento	
+	
+	if categoria == Category.INDUSTRIAL:
+		if categoria_vecina == Category.RECREATIVO:
+			money_changed = resta_grande
+		if categoria_vecina == Category.RESIDENCIAL:
+			money_changed = resta_grande
+		if categoria_vecina == Category.COMERCIAL:
+			money_changed = aumento
+	
+	if categoria == Category.COMERCIAL:
+		if categoria_vecina == Category.RECREATIVO:
+			money_changed = aumento_grande
+		if categoria_vecina == Category.INDUSTRIAL:
+			money_changed = aumento
+		if categoria_vecina == Category.RESIDENCIAL:
+			money_changed = aumento
+			
+	if categoria == Category.RECREATIVO:
+		if categoria_vecina == Category.COMERCIAL:
+			money_changed = aumento_grande
+		if categoria_vecina == Category.INDUSTRIAL:
+			money_changed = resta_grande
+		if categoria_vecina == Category.RESIDENCIAL:
+			money_changed = aumento_grande
+			
+	return money_changed
--- Tile.gd
+++ Tile.gd
@@ -1,6 +1,8 @@
 class_name Tile
 extends Node3D
 
+@export var money_gain : float
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
