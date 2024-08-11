f5ddc28b5c7854df1d6c8893a14db50fc7a534df
koso <sasukoso11@hotmail.com>
2024-08-11 19:11:08 -0300

    fixeado contorno rojo por default

--- scenes/tile.gd
+++ scenes/tile.gd
@@ -11,7 +11,8 @@ signal money_gained_changed(value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print(money_gain)
+	money_gained_changed.emit(money_gain)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
